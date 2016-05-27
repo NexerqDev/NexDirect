d0eed2471841463e0304e1549727b5ef7f91a0d1
Nicholas Tay <nexerq@gmail.com>
2016-05-27 18:05:19 +1000

    Single instance WPF using class from MS blog

--- WpfNexDirect/App.xaml.cs
+++ WpfNexDirect/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Shell;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +12,40 @@ namespace NexDirect
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        // http://blogs.microsoft.co.il/arik/2010/05/28/wpf-single-instance-application/
+        private const string AppUnique = "NexDirect_SIApp";
+
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(AppUnique))
+            {
+                var app = new App();
+
+                app.InitializeComponent();
+                app.Run();
+
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        #region ISingleInstanceApp Members
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // focus
+            if (MainWindow.WindowState == WindowState.Minimized)
+            {
+                MainWindow.WindowState = WindowState.Normal;
+            }
+
+            MainWindow.Activate();
+
+            return true;
+        }
+
+        #endregion
     }
 }
