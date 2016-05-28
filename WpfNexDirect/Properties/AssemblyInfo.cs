2c8f754ebc5882eade6fe0237f37de9dc6f894a4
Nicholas Tay <nexerq@gmail.com>
2016-05-28 15:41:50 +1000

    Update some assembly info

--- WpfNexDirect/Properties/AssemblyInfo.cs
+++ WpfNexDirect/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NexDirect")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A free, open-source alternative osu! beatmap downloader.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NexDirect")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Nicholas Tay 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
