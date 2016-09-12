48b86de7e4c329e8fffd84675ffa2767f8ceb44d
Nicholas Tay <nexerq@gmail.com>
2016-09-12 11:05:22 +1000

    Nicer direct download dialog

---
+++ NexDirect/Dialogs/DirectDownload.xaml.cs
@@ -0,0 +1,57 @@
+﻿using NexDirectLib;
+using NexDirectLib.Structures;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NexDirect.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for DirectDownload.xaml
+    /// </summary>
+    public partial class DirectDownload : Window
+    {
+        BeatmapSet set;
+        MainWindow _mw;
+
+        public DirectDownload(MainWindow mw, BeatmapSet bs)
+        {
+            InitializeComponent();
+
+            set = bs;
+            _mw = mw;
+
+            Title = $"{set.Artist} - {set.Title} <{set.Mapper}> :: NexDirect Download";
+            previewImage.Source = new BitmapImage(bs.PreviewImage);
+            songInfoLabel.Content = $"{set.Artist} - {set.Title} (mapped by {set.Mapper})";
+        }
+
+        private void downloadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AudioManager.ForceStopPreview();
+            _mw.DownloadBeatmapSet(set);
+            Close();
+        }
+
+        private void previewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Osu.PlayPreviewAudio(set);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start($"https://osu.ppy.sh/s/{set.Id}");
+        }
+    }
+}
