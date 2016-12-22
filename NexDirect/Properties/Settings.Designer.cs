87700b12d16995b104d5b2b06e8fde73b4179b3d
Nicholas Tay <nexerq@gmail.com>
2016-12-22 16:18:57 +1100

    Minimize to tray true default

--- NexDirect/Properties/Settings.Designer.cs
+++ NexDirect/Properties/Settings.Designer.cs
@@ -157,7 +157,7 @@ namespace NexDirect.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool minimizeToTray {
             get {
                 return ((bool)(this["minimizeToTray"]));
