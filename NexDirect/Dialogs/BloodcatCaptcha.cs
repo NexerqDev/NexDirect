b50d6acb1103fd4fd934b6b26a4434dfeb961bd5
Nicholas Tay <nexerq@gmail.com>
2017-05-31 22:51:11 +1000

    Switch to gecko, but not like it helps

--- NexDirect/Dialogs/BloodcatCaptcha.cs
+++ NexDirect/Dialogs/BloodcatCaptcha.cs
@@ -1,6 +1,4 @@
-﻿using CefSharp;
-using CefSharp.WinForms;
-using NexDirectLib;
+﻿using NexDirectLib;
 using NexDirectLib.Structures;
 using System;
 using System.Collections.Generic;
@@ -12,13 +10,14 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Gecko;
 
 namespace NexDirect.Dialogs
 {
     public partial class BloodcatCaptcha : Form
     {
         private static BloodcatCaptcha _this;
-        private ChromiumWebBrowser browser;
+        private GeckoWebBrowser browser;
 
         public BloodcatCaptcha(BeatmapSet set)
         {
@@ -26,76 +25,28 @@ namespace NexDirect.Dialogs
 
             _this = this;
 
-            // make our chrome browser
-            if (!Cef.IsInitialized)
-            {
-                CefSettings settings = new CefSettings();
-                Cef.Initialize(settings);
-            }
-
-            browser = new ChromiumWebBrowser("http://bloodcat.com/osu/s/" + set.Id);
+            // make our gecko browser
+            browser = new GeckoWebBrowser();
             this.Controls.Add(browser);
             browser.Dock = DockStyle.Fill;
 
-            browser.DownloadHandler = new downloadHandle();
+            Gecko.LauncherDialog.Download += downloadHandler;
+            browser.Navigate("http://bloodcat.com/osu/s/" + set.Id);
         }
 
-        // when it tries that means its done
-        private class downloadHandle : IDownloadHandler
+        private void downloadHandler(object sender, LauncherDialogEvent e)
         {
-            public void OnBeforeDownload(IBrowser browser, DownloadItem downloadItem, IBeforeDownloadCallback callback)
+            // we're not actually interested in download, just cookie hijack
+            using (var cookies = CookieManager.GetEnumerator())
             {
-                // really just dont do jack shit about the download
-                Cef.GetGlobalCookieManager().VisitAllCookies(new bloodcatCookieHandle());
-                _this.Invoke(new Action(() => _this.Close()));
-            }
-
-            public void OnDownloadUpdated(IBrowser browser, DownloadItem downloadItem, IDownloadItemCallback callback) { }
-        }
-
-        private class bloodcatCookieHandle : ICookieVisitor
-        {
-            public bool Visit(Cookie cookie, int count, int total, ref bool deleteCookie)
-            {
-                // just transfer all cookies over to handle
-                Bloodcat.Cookies.Add(new System.Net.Cookie(cookie.Name, cookie.Value, cookie.Path, cookie.Domain));
-                return true;
-            }
-
-            #region IDisposable Support
-            private bool disposedValue = false; // To detect redundant calls
-
-            protected virtual void Dispose(bool disposing)
-            {
-                if (!disposedValue)
+                while (cookies.MoveNext()) // loop thru
                 {
-                    if (disposing)
-                    {
-                        // TODO: dispose managed state (managed objects).
-                    }
-
-                    // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                    // TODO: set large fields to null.
-
-                    disposedValue = true;
+                    Cookie cookie = cookies.Current;
+                    Bloodcat.Cookies.Add(new System.Net.Cookie(cookie.Name, cookie.Value, cookie.Path, cookie.Host));
                 }
             }
 
-            // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-            // ~bloodcatCookieHandle() {
-            //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            //   Dispose(false);
-            // }
-
-            // This code added to correctly implement the disposable pattern.
-            public void Dispose()
-            {
-                // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-                Dispose(true);
-                // TODO: uncomment the following line if the finalizer is overridden above.
-                // GC.SuppressFinalize(this);
-            }
-            #endregion
+            Close();
         }
     }
 }
